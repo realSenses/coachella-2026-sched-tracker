3640feb3ee4669db0760d5c7047658f9094120dd
realSenses <realSenses0@gmail.com>
2026-04-11 00:56:58 -0600

    updates

---
+++ sw.js
@@ -0,0 +1,31 @@
+const CACHE = 'coachella2026-v1';
+const ASSETS = [
+  'index.html',
+  'manifest.json',
+  'fonts/JTUSjIg69CK48gW7PXooxW4.ttf',
+  'fonts/aFTR7PB1QTsUX8KYvrGyIYQ.ttf',
+  'fonts/aFTT7PB1QTsUX8KYth-orYataA.ttf',
+  'fonts/aFTU7PB1QTsUX8KYhh0.ttf',
+  'icons/icon-192.png',
+  'icons/icon-512.png',
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request))
+  );
+});
